b03be6794518113265e40e2b898af52a0e2e5de3
ahmed <ahabib@itemis.com>
2022-11-09 12:58:33 +0100

    Initial commit

---
+++ src/main/java/me/ahabib/jaxrswildflyoidckeycloak/HelloApplication.java
@@ -0,0 +1,9 @@
+package me.ahabib.jaxrswildflyoidckeycloak;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
